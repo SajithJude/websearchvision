a17b8020a81434cf31285e219c2a396f59fddc2f
SajithJude <judesajith.aj@gmail.com>
2022-12-15 04:04:46 +0530

    dsd

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ if url:
     # st.write("An exception occurred")
 
     new = pd.DataFrame(x)
-    ne = pd.DataFrame(st.json(responses))
+    ne = pd.DataFrame(responses)
     # n = pd.DataFrame(z)
     # wn = pd.DataFrame(w)
     # st.write("keywords  :"+str(f))
