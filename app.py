17a2eef42f681a82d7c55a88f4e8c3a7eefd02a8
SajithJude <judesajith.aj@gmail.com>
2022-12-15 02:54:31 +0530

    dsd

--- app.py
+++ app.py
@@ -2,24 +2,29 @@ import streamlit as st
 from googleapiclient.discovery import build
 import os
 
-APIKEY = os.environ["API_KEY"] 
-IMAGE="https://get.pxhere.com/photo/road-highway-advertising-travel-sign-community-usa-landmark-street-sign-attraction-historic-tourism-signage-road-sign-illinois-history-66-traffic-sign-route-66-mother-road-odell-644464.jpg"
 
-vservice = build('vision', 'v1', developerKey=APIKEY)
-request = vservice.images().annotate(body={
-        'requests': [{
-                'image': {
-                    'source': {
-                        'imageUri': IMAGE
-                    }
-                },
-                'features': [{
-                    'type': 'WEB_DETECTION'
-                }]
-            }],
-        })
-responses = request.execute(num_retries=2)
-x = responses['responses'][0]['webDetection']['fullMatchingImages']
+url = st.text_input("enter url")
 
-# print(js.formatter.format(responses))
-st.text(x)
+if st.button("submit"):
+    APIKEY = os.environ["API_KEY"] 
+    IMAGE= url
+    vservice = build('vision', 'v1', developerKey=APIKEY)
+    request = vservice.images().annotate(body={
+            'requests': [{
+                    'image': {
+                        'source': {
+                            'imageUri': IMAGE
+                        }
+                    },
+                    'features': [{
+                        'type': 'WEB_DETECTION'
+                    }]
+                }],
+            })
+    responses = request.execute(num_retries=2)
+    x = responses['responses'][0]['webDetection']['fullMatchingImages']
+
+    # print(js.formatter.format(responses))
+    st.text(x)
+    
+    
