fdbe9eb0d837d10f490bb998c260a38071e0ea5c
SajithJude <judesajith.aj@gmail.com>
2022-12-16 00:25:41 +0530

    dsd

--- pages/Web_camera_input.py
+++ pages/Web_camera_input.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import base64
+import requests
+import json
 
 img_file_buffer = st.camera_input("Take a picture")
 
@@ -8,3 +11,43 @@ if img_file_buffer is not None:
     # Check the type of bytes_data:
     # Should output: <class 'bytes'>
     st.write(type(bytes_data))
+    
+
+    # The GCP Vision API URL 
+    vision_url = 'https://vision.googleapis.com/v1/images:annotate?key='
+
+    # Your Google Cloud Platform (GCP) API KEY. Generate one on cloud.google.com
+    api_key = os.environ["API_KEY"] 
+    # Load your image as a base64 encoded string
+
+
+    
+    encoded_image = base64.b64encode(img_file_buffer.read())
+
+    # Generate a post request for GCP vision Annotation
+    json_data= {
+        'requests': [
+            {
+                'image':{
+                    'content': encoded_image.decode('utf-8')
+                },
+                'features':[
+                    {
+                        'type':'WEB_DETECTION',
+                        'maxResults':5
+                    }
+                ]
+            }
+        ]
+    }
+
+    # Handle the API request
+    response = requests.post(vision_url+api_key, json=json_data)
+
+    # Read the response in json format
+    response_data = response.json()
+
+    # Printing the response, in this case it will return all the labels that are 
+    # identified in the image
+    st.write("GCP Vision Annotation :")
+    st.write(json.dumps(response_data, indent=4))
