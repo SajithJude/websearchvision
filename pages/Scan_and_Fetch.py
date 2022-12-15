bee7c6e1c5f3a7cfa6e068f0094994aa6155ef5b
SajithJude <judesajith.aj@gmail.com>
2022-12-16 05:11:24 +0530

    sds

--- pages/Scan_and_Fetch.py
+++ pages/Scan_and_Fetch.py
@@ -93,13 +93,16 @@ else:
         col1 ,col2 = st.columns(2)
 
         with col1:
-            st.header("Image annotation :")
+            st.header("Image annotation")
+            st.write(gl)
+
             # st.write(annotation['surpriseLikelihood'])
 
             # st.image("https://static.streamlit.io/examples/cat.jpg")
 
         with col2:
-            st.header(gl)
+            st.header("Detected Entities")
+            st.write(entities)
             # st.write(annotation['joyLikelihood']) 
         # st.write(gl)
 
