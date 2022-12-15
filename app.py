7b6ea56e24d296a0117d77ac0ea4ace320dc78bf
SajithJude <judesajith.aj@gmail.com>
2022-12-16 00:21:53 +0530

    dsd

--- app.py
+++ app.py
@@ -1,7 +1,57 @@
-import streamlit as st
-from googleapiclient.discovery import build
-import os
-import pandas as pd
+# import streamlit as st
+# from googleapiclient.discovery import build
+# import os
+# import pandas as 
+
+
+
+
+# python code for latest way to annotate images using gcp vision v-1 without using types.
+
+
+# Importing necessary Standard Libraries
+import base64
+import requests
+import json
+
+# # The GCP Vision API URL 
+# vision_url = 'https://vision.googleapis.com/v1/images:annotate?key='
+
+# # Your Google Cloud Platform (GCP) API KEY. Generate one on cloud.google.com
+# api_key = os.environ["API_KEY"] 
+# # Load your image as a base64 encoded string
+
+
+# with open("/content/images.jpg", "rb") as image_file:
+#     encoded_image = base64.b64encode(image_file.read())
+
+# # Generate a post request for GCP vision Annotation
+# json_data= {
+#     'requests': [
+#         {
+#             'image':{
+#                 'content': encoded_image.decode('utf-8')
+#             },
+#             'features':[
+#                 {
+#                     'type':'WEB_DETECTION',
+#                     'maxResults':5
+#                 }
+#             ]
+#         }
+#     ]
+# }
+
+# # Handle the API request
+# response = requests.post(vision_url+api_key, json=json_data)
+
+# # Read the response in json format
+# response_data = response.json()
+
+# # Printing the response, in this case it will return all the labels that are 
+# # identified in the image
+# print("GCP Vision Annotation :")
+# print(json.dumps(response_data, indent=4))
 
 
 url = st.text_input("enter url")
---
+++ pages/Web_camera_input.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+img_file_buffer = st.camera_input("Take a picture")
+
+if img_file_buffer is not None:
+    # To read image file buffer as bytes:
+    bytes_data = img_file_buffer.getvalue()
+    # Check the type of bytes_data:
+    # Should output: <class 'bytes'>
+    st.write(type(bytes_data))
