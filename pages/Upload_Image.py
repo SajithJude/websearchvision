0a061ac9c2bba8a63e36eccdc360e20627478cef
SajithJude <judesajith.aj@gmail.com>
2022-12-16 06:28:13 +0530

    dsd

--- pages/Upload_Image.py
+++ pages/Upload_Image.py
@@ -97,7 +97,7 @@ else:
             openai.api_key =  os.getenv("OPENAI_API_KEY")
             resp = openai.Completion.create(
             model="text-davinci-002",
-            prompt="Find some information about this medicine while also mentioning the trade name of the other drugs that has interactions with this  : " + info ,
+            prompt="Fetch information about this medicine, and use its cases, while also mentioning the trade name of the other drugs that has interactions with this  : " + info ,
             temperature=0.2,
             max_tokens=3500,
             top_p=1,
@@ -107,10 +107,6 @@ else:
             )
             st.write(resp.choices[0].text)
 
-            # st.table(pageinfo)
-            # st.table(result['responses'][0])
-
-            # st.write(result)
 
         except: 
             st.write("An exception occurred")
