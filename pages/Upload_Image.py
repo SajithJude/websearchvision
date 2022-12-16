2d9070adc98a8a2c62435f2515178d10b896c106
SajithJude <judesajith.aj@gmail.com>
2022-12-16 06:37:00 +0530

    sdd

--- pages/Upload_Image.py
+++ pages/Upload_Image.py
@@ -97,7 +97,7 @@ else:
             openai.api_key =  os.getenv("OPENAI_API_KEY")
             resp = openai.Completion.create(
             model="text-davinci-002",
-            prompt="Fetch information about this medicine, and use its uses, include names drugs that cross interacts with this  : " + info ,
+            prompt="Fetch comprehensive information about this medicine, and specify the symptoms that it treats: " + info ,
             temperature=0.2,
             max_tokens=3500,
             top_p=1,
