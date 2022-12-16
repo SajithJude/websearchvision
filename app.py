23347a8f58121e62acc9846736ffdbcb51de237d
SajithJude <judesajith.aj@gmail.com>
2022-12-16 06:58:17 +0530

    edited home page

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ import streamlit as st
 st.title("Ïnstancy Demo Gallery")
 
 
-st.write(
+st.subheader(
         """
         **Read Me**: 👆 Click on the arrow icon on the right to toggle the navigationa bar and access the demo use cases
-        **Available Demos**: Scan and Fetch, OCR Text Detection, Drug Scanner, Emotion Detector .
-        all the above demos will request webcamera permissions to process video or you can upload an image and run the inference as well !
+
         """
     )
+
+st.subheader(
+    """
+    **Available Demos**: Scan and Fetch, OCR Text Detection, Drug Scanner, Emotion Detector .
+        all the above demos will request webcamera permissions to process video or you can upload an image and run the inference as well !
+    """
+)
