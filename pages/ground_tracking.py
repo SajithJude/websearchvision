2cbd33e9031c3a12e9f0824d31452942444c02ff
SajithJude <judesajith.aj@gmail.com>
2022-12-19 12:24:03 +0530

    dsds

---
+++ pages/ground_tracking.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import streamlit.components.v1 as components
+# container_width = st.get_container_width
+# width = st.javascript('window.screen.width')
+st.set_page_config(layout="wide")
+# embed streamlit docs in a streamlit app
+components.iframe("https://sajithjude.github.io/GroundTracking/",height=400)
