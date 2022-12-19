d5b6ce48e72898ec961585c8b0d73453ce5f9c83
SajithJude <judesajith.aj@gmail.com>
2022-12-19 13:38:06 +0530

    sdds

--- pages/1_Image_tracking.py
+++ pages/1_Image_tracking.py
@@ -4,9 +4,14 @@ from PIL import Image
 # container_width = st.get_container_width
 # width = st.javascript('window.screen.width')
 st.set_page_config(layout="wide")
-image2 = Image.open('pages/ground.png')
+image = Image.open('pages/frame.png')
 
-st.title("Scan the QR code to view the Ground Tracking Demo")
-st.image(image2)
+# st.title("Scan the QR code to view the demo")
+st.image(image)
+st.subheader("To View, open camera on smartphone and scan code")
+st.caption("or visit")
+st.write("https://sajithjude.github.io/instancy")
+st.caption("on smartphone or tablet")
+
 # embed streamlit docs in a streamlit app
 # components.iframe("https://sajithjude.github.io/instancy/",height=400)
--- pages/2_ground_tracking.py
+++ pages/2_ground_tracking.py
@@ -4,9 +4,9 @@ from PIL import Image
 # container_width = st.get_container_width
 # width = st.javascript('window.screen.width')
 st.set_page_config(layout="wide")
-image = Image.open('pages/frame.png')
+image2 = Image.open('pages/ground.png')
 
-st.title("Scan the QR code to view the demo")
-st.image(image)
+st.title("Scan the QR code to play Ground Tracking Demo")
+st.image(image2)
 # embed streamlit docs in a streamlit app
 # components.iframe("https://sajithjude.github.io/instancy/",height=400)
