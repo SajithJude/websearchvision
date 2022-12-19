d8acae382c8526b72e427fe7c161c38cd717ac5f
SajithJude <judesajith.aj@gmail.com>
2022-12-19 15:15:39 +0530

    dsds

--- pages/10_Language_Translation.py
+++ pages/10_Language_Translation.py
@@ -177,7 +177,7 @@ if __name__ == "__main__":
                 response = requests.get(requestUrl)
 
                 # Get the translated text from the response 
-                translatedText = response.json()
+                translatedText = response.json()[0][0][0]
                 
                 # Print the translated text 
                 st.write(translatedText)
