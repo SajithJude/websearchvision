59c4ab6640ba7bf226490743b56ff705069bd1ab
SajithJude <judesajith.aj@gmail.com>
2022-12-19 12:33:25 +0530

    dsds

--- pages/Image_tracking.py
+++ pages/Image_tracking.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import streamlit.components.v1 as components
+from PIL import Image
 # container_width = st.get_container_width
 # width = st.javascript('window.screen.width')
 st.set_page_config(layout="wide")
+image = Image.open('pages\frame.png')
+
+st.title("Scan the QR code to view the demo")
+st.image(image)
 # embed streamlit docs in a streamlit app
 components.iframe("https://sajithjude.github.io/instancy/",height=400)
