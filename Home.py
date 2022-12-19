1d7710f13a048ef3755b15b74e2851fee4f01b14
SajithJude <judesajith.aj@gmail.com>
2022-12-19 17:49:48 +0530

    dsd

--- Home.py
+++ Home.py
@@ -13,19 +13,19 @@ def get_base64_of_bin_file(bin_file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
-    <style>
-    body {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
+# def set_png_as_page_bg(png_file):
+# #     bin_str = get_base64_of_bin_file(png_file)
+# #     page_bg_img = '''
+# #     <style>
+# #     body {
+# #     background-image: url("data:image/png;base64,%s");
+# #     background-size: cover;
+# #     }
+# #     </style>
+#     ''' % bin_str
     
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
+#     st.markdown(page_bg_img, unsafe_allow_html=True)
+#     return
 
 
 
