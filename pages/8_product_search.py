be1c2869cfdf150831cbebd8f248a60278041d7e
SajithJude <judesajith.aj@gmail.com>
2022-12-19 16:06:48 +0530

    sdsd

--- pages/8_product_search.py
+++ pages/8_product_search.py
@@ -74,7 +74,7 @@ else:
             try:
                 linkedInUrl = [site for site in result['responses'][0]['webDetection']['pagesWithMatchingImages'] if site['url'].__contains__('amazon.com')]
                 st.subheader("Amazon product matches")
-                inlik = map(lambda linkedInUrl: linkedInUrl[0]['url'], linkedInUrl)
+                inlik = map(lambda pageinfo: linkedInUrl[0]['url'], linkedInUrl)
                 for i in list(inlik):
                     st.write(i)
                 if len(inlik)==0:
