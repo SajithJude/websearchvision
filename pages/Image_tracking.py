b060838f32aa0c3da1c3c49e9e56b754b624d99c
SajithJude <judesajith.aj@gmail.com>
2022-12-19 12:11:55 +0530

    jhjghj

--- pages/Image_tracking.py
+++ pages/Image_tracking.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 # container_width = st.get_container_width
-width = st.javascript('window.screen.width')
+# width = st.javascript('window.screen.width')
 st.set_page_config(layout="wide")
 # embed streamlit docs in a streamlit app
 components.iframe("https://sajithjude.github.io/instancy/",use_container_width=True, height=None)
