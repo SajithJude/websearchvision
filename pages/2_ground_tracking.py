f202e5ad4496c189e8ced4f34aa13f103f19fd03
SajithJude <judesajith.aj@gmail.com>
2022-12-19 13:42:16 +0530

    dsd

--- pages/2_ground_tracking.py
+++ pages/2_ground_tracking.py
@@ -6,7 +6,12 @@ from PIL import Image
 st.set_page_config(layout="wide")
 image2 = Image.open('pages/ground.png')
 
-st.title("Scan the QR code to play Ground Tracking Demo")
+
 st.image(image2)
+st.subheader("To View, open camera on smartphone and scan code")
+st.caption("or visit")
+st.write("https://sajithjude.github.io/GroundTracking")
+st.caption("on smartphone or tablet")
+
 # embed streamlit docs in a streamlit app
 # components.iframe("https://sajithjude.github.io/instancy/",height=400)
