a5b886d06c0971559150f2aecaf17c747021d087
SajithJude <judesajith.aj@gmail.com>
2022-12-19 12:05:33 +0530

    dsd

--- pages/Image_tracking.py
+++ pages/Image_tracking.py
@@ -3,4 +3,4 @@ import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 # embed streamlit docs in a streamlit app
-components.iframe("https://sajithjude.github.io/instancy/")
+components.iframe("https://sajithjude.github.io/instancy/",width=None, height=None, scrolling=False)
