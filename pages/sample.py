f6af225bbf3c4e556759703c949dd10d01625bc7
SajithJude <judesajith.aj@gmail.com>
2022-12-22 12:28:12 +0530

    sdds

--- pages/sample.py
+++ pages/sample.py
@@ -18,7 +18,7 @@ if img_file_buffer is not None:
     bytes_data = img_file_buffer.getvalue()
     # Check the type of bytes_data:
     # Should output: <class 'bytes'>
-    st.write(type(bytes_data))
+    # st.write(type(bytes_data))
     # The GCP Vision API URL 
     vision_url = 'https://vision.googleapis.com/v1/images:annotate?key='
 
@@ -83,7 +83,7 @@ if img_file_buffer is not None:
 
 
 
-    components.html(
+    st.markdown(
                     """
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
             <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -121,7 +121,7 @@ if img_file_buffer is not None:
             </div>
             </div>
                     """,
-                    height=400,
+                    unsafe_allow_html=True
                 )
 
 # if st.button("Clear me"):
