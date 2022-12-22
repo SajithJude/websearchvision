f94ec414210b2101314a5c54188b023eb1e879b9
SajithJude <judesajith.aj@gmail.com>
2022-12-22 12:18:20 +0530

    dsd

--- pages/sample.py
+++ pages/sample.py
@@ -89,8 +89,8 @@ components.html(
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <div id="accordion">
 <div class="card">
-    {{x}}
-    sample info
+    {x}
+    
     <!-- <div class="card-header" id="headingOne">
     <h5 class="mb-0">
         <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
