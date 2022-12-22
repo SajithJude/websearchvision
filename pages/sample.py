9edc381c0a402509441383fd60bf59d9e545220f
SajithJude <judesajith.aj@gmail.com>
2022-12-22 12:31:07 +0530

    dsd

--- pages/sample.py
+++ pages/sample.py
@@ -83,7 +83,7 @@ if img_file_buffer is not None:
 
 
 
-    st.write(
+    st.write(st.markdown(
                    f"""
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
             <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -122,7 +122,7 @@ if img_file_buffer is not None:
             </div>
                     """,
                     unsafe_allow_html=True
-                )
+                ))
 
 # if st.button("Clear me"):
 #     placeholder.empty()
