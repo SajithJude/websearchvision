cb3b43f107c9427ec4f7e343d051037075b610a6
SajithJude <judesajith.aj@gmail.com>
2022-12-22 11:20:55 +0530

    dsd

---
+++ pages/sample.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+expanded = False
+
+def render():
+  global expanded
+  if expanded:
+    st.markdown(
+      """
+      <div class="footer">
+        <p>&copy; 2020 Company</p>
+        <div>
+          <a href="www.company.com/about">About</a>
+          <a href="www.company.com/contact">Contact</a>
+        </div>
+      </div>
+      """
+    , unsafe_allow_html=True)
+  else:
+    st.markdown(
+      """
+      <div class="footer">
+        <p>&copy; 2020 Company</p>
+      </div>
+      """
+    , unsafe_allow_html=True)
+    if st.button('Expand Footer'):
+      expanded = True
+
+render()
