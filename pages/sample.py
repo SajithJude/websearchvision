5962437f40ece89675c8d2bec8b3fdf03a89a4d6
SajithJude <judesajith.aj@gmail.com>
2022-12-22 12:35:59 +0530

    sdsd

--- pages/sample.py
+++ pages/sample.py
@@ -89,22 +89,7 @@ if img_file_buffer is not None:
             <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
             <div id="accordion">
-            <div class="card">
-                {annotation['joyLikelihood']}
-                
-                <!-- <div class="card-header" id="headingOne">
-                <h5 class="mb-0">
-                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                    Collapsible Group Item #1
-                    </button>
-                </h5>
-                </div>
-                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                <div class="card-body">
-                    Collapsible Group Item #1 content
-                </div>
-                </div> -->
-            </div>
+    
             <div class="card">
                 <div class="card-header" id="headingTwo">
                 <h5 class="mb-0">
@@ -115,7 +100,7 @@ if img_file_buffer is not None:
                 </div>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
                 <div class="card-body">
-                    Collapsible Group Item #2 content
+                      {annotation['joyLikelihood']}
                 </div>
                 </div>
             </div>
