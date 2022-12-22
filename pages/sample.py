9c01fafd61c59d915635bd1a871ca29f45f27b48
SajithJude <judesajith.aj@gmail.com>
2022-12-22 11:31:38 +0530

    dsd

--- pages/sample.py
+++ pages/sample.py
@@ -1,32 +1,47 @@
 import streamlit as st
-
 import streamlit.components.v1 as components
 
+placeholder = st.empty()
 
-components.html(
-  """
-<div>
-  <div style="display: flex; flex-direction: column; align-items: center;">
-    <button id="container-expand" style="background-color: #3399FF; color: white; padding: 10px; border-radius: 5px; font-size: 1rem; margin: 8px;"> Expand Container </button>
-    <div id="container" style="display: flex; width: 100%; max-height: 0px; overflow: hidden; transition: max-height 0.5s ease-out;">
-      <!-- content of the container goes here -->
-    </div>
-  </div>
-</div>
+with placeholder:
+    components.html(
+        """
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <div id="accordion">
+        <div class="card">
+            <div class="card-header" id="headingOne">
+            <h5 class="mb-0">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Collapsible Group Item #1
+                </button>
+            </h5>
+            </div>
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+                Collapsible Group Item #1 content
+            </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+                <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Collapsible Group Item #2
+                </button>
+            </h5>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body">
+                Collapsible Group Item #2 content
+            </div>
+            </div>
+        </div>
+        </div>
+        """,
+        height=200,
+    )
 
-<script>
-  const expandButton = document.getElementById('container-expand');
-  const container = document.getElementById('container');
-
-  expandButton.addEventListener('click', () => {
-    if (container.style.maxHeight === '0px') {
-      container.style.maxHeight = '100vh';
-    } else {
-      container.style.maxHeight = '0px';
-    }
-  });
-</script>
-
-
-  """
-)
+if st.button("Clear me"):
+    placeholder.empty()
