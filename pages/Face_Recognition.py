f314b214f30a9c68d0710281e1f6862ad0cd00d0
SajithJude <judesajith.aj@gmail.com>
2022-12-24 13:06:30 +0530

    sd

--- pages/Face_Recognition.py
+++ pages/Face_Recognition.py
@@ -1,13 +1,13 @@
-import streamlit as st
-import streamlit.components.v1 as components
+# import streamlit as st
+# import streamlit.components.v1 as components
 
-placeholder = st.empty()
+# placeholder = st.empty()
 
-import streamlit as st
-import base64
-import requests
-import json
-import os
+# import streamlit as st
+# import base64
+# import requests
+# import json
+# import os
 
 # st.title("Face Emotion Detection")
 # st.caption("Point the camera to a persons photo, the face of a real person, or your own face to detect the emotions.​")
@@ -82,35 +82,164 @@ import os
          
 
 
+import streamlit as st
+import base64
 
-st.write(
-                   f"""
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-            <div id="accordion" style="background: linear-gradient(353.6deg, rgba(39, 60, 117, 0.83) -28.41%, #192A56 96%);">
+from PIL import Image
 
-            <div class="card" style="box-sizing: border-box; height: 393px; background: rgba(25, 42, 86, 0.31); border-top: 1px solid #FFFFFF;">
-                    juhdfjkdfsak
-            </div>
-            <div class="card">
-                <div class="card-header" id="headingTwo">
-                <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                    Collapsible Group Item #2
-                    </button>
-                </h5>
-                </div>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                <div class="card-body">
-                    Collapsible Group Item #2 content
-                </div>
-                </div>
-            </div>
-            </div>
-                    """,
-                    unsafe_allow_html=True
-                )
+import streamlit as st
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
 
-# if st.button("Clear me"):
-#     placeholder.empty()
+# @st.cache(allow_output_mutation=True)
+# def get_base64_of_bin_file(bin_file):
+#     with open(bin_file, 'rb') as f:
+#         data = f.read()
+#     return base64.b64encode(data).decode()
+
+# def set_png_as_page_bg(png_file):
+# #     bin_str = get_base64_of_bin_file(png_file)
+# #     page_bg_img = '''
+# #     <style>
+# #     body {
+# #     background-image: url("data:image/png;base64,%s");
+# #     background-size: cover;
+# #     }
+# #     </style>
+#     ''' % bin_str
+    
+#     st.markdown(page_bg_img, unsafe_allow_html=True)
+#     return
+
+
+
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style))
+
+
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+
+def layout(*args):
+
+    style = """
+    <style>
+      # MainMenu {visibility: hidden;}
+      footer {visibility: hidden;}
+     .stApp { bottom: 105px; }
+     # body 
+    </style>
+    """
+
+    style_div = styles(
+        position="fixed",
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        color="#192A56",
+        text_align="center",
+        height="auto",
+        opacity=1
+    )
+
+    style_hr = styles(
+        display="block",
+        margin=px(8, 8, "auto", "auto"),
+        border_style="inset",
+        border_width=px(2)
+    )
+
+    body = p()
+    foot = div(
+        style=style_div
+    )(
+        hr(
+            style=style_hr
+        ),
+        body
+    )
+
+    st.markdown(style, unsafe_allow_html=True)
+
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+
+def footer():
+    myargs = [
+        "Made with ❤️ from ",
+        
+        link("www.instancy.com","Instancy Inc"),
+        br(),
+    ]
+    layout(*myargs)
+
+
+if __name__ == "__main__":
+    
+
+    im = Image.open("images.png")
+    st.set_page_config(
+        page_title="Instancy",
+        page_icon=im,
+        layout="wide",
+    )
+    # st.set_theme('primary_color', primary_color='#BFD731')
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.write('     ')
+
+    with col2:
+        st.title("Home")
+
+    with col3:
+        st.write('     ')
+
+    cols1, cols2, cols3 = st.columns(3)
+
+    with cols1:
+        st.write(' ')
+
+    with cols2:
+        st.image('https://cdn-apinb.nitrocdn.com/LGFQTZTBRQFYZkDHnBAkeTYvUEPBCNKO/assets/static/optimized/rev-f886686/wp-content/uploads/2022/11/logo-instancy.png')
+
+
+    with cols3:
+        st.write(' ') 
+
+
+
+
+    st.subheader(
+        """
+Augmented Reality Demos
+        """
+    )
+
+    
+
+
+    st.caption(
+    """
+    Click on the Menu to select an augmented reality Scenario.
+    """
+)
+
+    # st.write("www.instancy.com")
+    footer()
+
+
+
+
+
+
