b3432d5304f5a2c2976c29386d07f2e5de7e80ad
SajithJude <judesajith.aj@gmail.com>
2022-12-24 14:39:01 +0530

    dsd

--- pages/parent.py
+++ pages/parent.py
@@ -86,7 +86,8 @@ def layout(*args):
         <style>
         div.stButton > button:first-child {
             background-color: #BFD731;
-            font-weight: bold
+            font-weight: bold;
+            font-size:20px;
             color:#000000;
             border-radius:20px 20px 20px 20px; 
         }
