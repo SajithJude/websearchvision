d506abc758bdf79073a2f86016f89555fe359df5
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2022-12-24 12:33:19 +0000

    dsd

--- pages/parent.py
+++ pages/parent.py
@@ -82,10 +82,8 @@ def layout(*args):
     body = div(
        
         style=styles(
-        position="relative",
-        left=0,
-        bottom=0,
-        width=percent(10),
+        position="absolute",
+        width=percent(100),
         color="#192A56",
         text_align="center",
         opacity=1
