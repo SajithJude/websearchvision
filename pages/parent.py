0094e29e141e3640ba1f82cad4e4683ae4f2d5e6
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2022-12-24 13:09:50 +0000

    jjj

--- pages/parent.py
+++ pages/parent.py
@@ -25,7 +25,7 @@ m = st.markdown("""
             background-color: #EBEBEB;
             color:#ffffff;
             }
-        div.element-container.css-1qipp0u.e1tzin5v3 {
+        div.element-container {
             width : 50%;
         }
         
