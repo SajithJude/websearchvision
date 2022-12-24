bf9fbe9b5a23c432c95a661d240bb25c1b794b26
SajithJude <judesajith.aj@gmail.com>
2022-12-24 15:55:30 +0530

    dsd

--- pages/parent.py
+++ pages/parent.py
@@ -38,11 +38,10 @@ def layout(*args):
         # left=0,
         bottom=0,
         margin=px(0, 0, 0, 0),
-        width=percent(100),
+        width=percent(50),
         color="#192A56",
         text_align="center",
         height=px(380),
-        width=px(393),
         opacity=1
     )
 
